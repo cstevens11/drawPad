a03cfd6397f42423f23d3e00ecaccf23cabe908a
cstevens <colestevens24@gmail.com>
2022-08-03 00:07:18 -0700

    add color match function and live pen color display

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 let color = "black";
 let penStatus = "off";
+let colorMatchMode = "off";
 
 const sliderValue = document.querySelector(".sliderValue");
 
@@ -33,6 +34,8 @@ let makeGrid = function (size) {
         square.addEventListener('mousedown', togglePenON);
         square.addEventListener('mouseup', togglePenOFF);
         square.addEventListener('mouseover', squareColor);
+        square.addEventListener('click', copyColor);
+
         square.classList.add("drawBox");
         mainBox.appendChild(square)
     }
@@ -40,6 +43,20 @@ let makeGrid = function (size) {
 
 let changeColor = function (choice) {
     color = choice;
+    updateCurrentColor();
+}
+
+let updateCurrentColor = function () {
+    const currentColor = document.querySelector(".currentPenColor");
+    if (color === "random") {
+        currentColor.textContent = "Random"
+        currentColor.style.background = "linear-gradient(to left, violet, indigo, blue, green, yellow, orange, red)";
+    } else {
+        currentColor.textContent = "";
+        currentColor.style.background = color;
+
+    }
+
 }
 
 let resetGrid = function () {
@@ -58,6 +75,40 @@ let togglePenOFF = function () {
     console.log (penStatus)
 }
 
+let copyColorON = function () {
+    colorMatchMode = "on"
+}
+
+let copyColorOFF = function () {
+    colorMatchMode = "off"
+}
+
+// let cursorCheck = function () {
+//     const cursorType = document.getElementsByTagName("body");
+//     let currentCursor = cursorType.style.cursor;
+
+//     console.log(currentCursor)
+
+// }
+
+
+let colorMatch = function () {
+    // should change cursor to paint dropper and add event listener for the next click to take the 
+    // background color of the event of click
+    const cursor = document.querySelector(".cursor");
+    cursor.style.cursor = "crosshair";
+    copyColorON();
+
+}
+
+let copyColor = function () {
+    if (colorMatchMode === "on") {
+        changeColor(this.style.backgroundColor);
+        copyColorOFF();
+        const cursor = document.querySelector(".cursor");
+        cursor.style.cursor = "default";
+    }
+}
 
 
 let squareColor = function () {
@@ -72,7 +123,7 @@ let squareColor = function () {
         else {
             this.style.backgroundColor = color;
         }
-    }  else {
+    } else {
         return;
     }
 }
