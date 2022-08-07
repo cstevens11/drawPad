1a3edb0ec70e7cf4ca4909979d99ec35cc7c6c44
cstevens <colestevens24@gmail.com>
2022-08-07 14:55:22 -0700

    clean up JS code

--- javascript.js
+++ javascript.js
@@ -1,55 +1,19 @@
 let color = "black";
+
 let penStatus = false;
-let colorMatchMode = false;
-
-const sliderValue = document.querySelector(".sliderValue");
-
-
-
-
-let changeSize = function (input) {
-    if(input >= 2 && input <= 100){
-        makeGrid(input);
-    } else {
-        alert("Value must be between 2 and 100")
-    }
-}
-
-
 document.body.onmousedown = () => (penStatus = true)
 document.body.onmouseup = () => (penStatus = false)
 
-
-
-let makeGrid = function (size) {
-    const mainBox = document.querySelector(".mainBox");
-    const squares = mainBox.querySelectorAll("div");
-    squares.forEach((div) => div.remove());
-    mainBox.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-    mainBox.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-
-    let amount = size * size;
-
-   
-    
-    for (let i = 0; i < amount; i++) {
-        const square = document.createElement("div");
-        // square.addEventListener('mousedown', togglePenON);
-        // square.addEventListener('mouseup', togglePenOFF);
-        square.addEventListener('mouseover', squareColor);
-        square.addEventListener('mousedown', squareColor);
-        square.addEventListener('click', copyColor);
-        square.classList.add("drawBox");
-        square.style.backgroundColor = "whitesmoke"
-        mainBox.appendChild(square)
-    }
-};
-
-let changeColor = function (choice) {
-    color = choice;
-    updateCurrentColor(choice);
+let colorMatchMode = false;
+let copyColorON = function () {
+    colorMatchMode = true;
+}
+let copyColorOFF = function () {
+    colorMatchMode = false;
 }
 
+// SETTINGS COLLUMN FUNCTIONS
+// COLORTRACKER FUNCTIONS
 let updateCurrentColor = function () {
     const currentColor = document.querySelector(".currentPenColor");
     if (color === "random") {
@@ -63,48 +27,67 @@ let updateCurrentColor = function () {
 
 }
 
-let resetGrid = function () {
-    const mainBox = document.querySelector(".mainBox");
-    const squares = mainBox.querySelectorAll("div");
-    squares.forEach((div) => div.style.backgroundColor = "whitesmoke");
+// SLIDECONTAINER FUNCTIONS
+let changeSize = function (input) {
+    if(input >= 2 && input <= 100){
+        makeGrid(input);
+    } else {
+        alert("Value must be between 2 and 100")
+    }
 }
 
-let togglePenON = function (evt) {
-    evt.preventDefault();
-    penStatus = true;
-    console.log (penStatus);
+let liveSliderValue = function (value) {
+    const sliderValue = document.querySelector(".sliderValue");
+    sliderValue.textContent = `Grid Size: ${value} x ${value}`;
 }
 
-let togglePenOFF = function (evt) {
-    evt.preventDefault();
-    penStatus = false;
-    console.log (penStatus);
+// CUSTOMCOLOR and CONTENTBUTTONS FUNCTIONS
+let changeColor = function (choice) {
+    color = choice;
+    updateCurrentColor(choice);
 }
-
-let copyColorON = function () {
-    colorMatchMode = true;
-}
-
-let copyColorOFF = function () {
-    colorMatchMode = false;
-}
-
-// let cursorCheck = function () {
-//     const cursorType = document.getElementsByTagName("body");
-//     let currentCursor = cursorType.style.cursor;
-
-//     console.log(currentCursor)
-
-// }
-
 
 let colorMatch = function () {
-    // should change cursor to paint dropper and add event listener for the next click to take the 
-    // background color of the event of click
     const cursor = document.querySelector(".cursor");
     cursor.style.cursor = "crosshair";
     copyColorON();
+}
 
+let resetGrid = function () {
+    const drawPad = document.querySelector(".drawPad");
+    const squares = drawPad.querySelectorAll("div");
+    squares.forEach((div) => div.style.backgroundColor = "whitesmoke");
+}
+
+// DRAWPAD FUNCTIONS
+let makeGrid = function (size) {
+    let amount = size * size;
+    const drawPad = document.querySelector(".drawPad");
+    const squares = drawPad.querySelectorAll("div");
+    squares.forEach((div) => div.remove());
+    drawPad.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    drawPad.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < amount; i++) {
+        const square = document.createElement("div");
+        square.addEventListener('mouseover', squareColor);
+        square.addEventListener('mousedown', squareColor);
+        square.addEventListener('click', copyColor);
+        square.classList.add("drawBox");
+        square.style.backgroundColor = "whitesmoke";
+        drawPad.appendChild(square);
+    }
+};
+
+let squareColor = function (evt) {
+    evt.preventDefault();
+    if (colorMatchMode === true) return;
+    if (evt.type === 'mouseover' && !penStatus) return;
+    if (color === 'random') {
+        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    } else {
+        this.style.backgroundColor = color;
+    }
 }
 
 let copyColor = function (evt) {
@@ -117,51 +100,4 @@ let copyColor = function (evt) {
     }
 }
 
-
-let squareColor = function (evt) {
-    evt.preventDefault();
-    if (colorMatchMode === true) return;
-    if (evt.type === 'mouseover' && !penStatus) return;
-    // if (penStatus === "on") {
-    if (color === 'random') {
-        this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
-    } 
-        // else if (color === 'colorChoice') {
-        //     color = document.getElementById('colorInputColor').value;
-        //     this.style.backgroundColor = color;
-        // }
-    else {
-        this.style.backgroundColor = color;
-    }
-    
-}
-
-let liveSliderValue = function (value) {
-    sliderValue.textContent = `Grid Size: ${value} x ${value}`;
-}
-
-
-
-
-
-
 makeGrid (16);
-
-
-
-
-// const drawBoxes = document.querySelectorAll(".drawBox")
-// drawBoxes.forEach(div=> div.addEventListener('click', draw));
-
-
-
-
-
-
-
-
-// let draw = function (e) {
-//     e.target.style.backgroundColor = '#fefefe'
-
-
-// }
